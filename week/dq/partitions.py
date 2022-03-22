1e52453b0d3d12461d2b39109719a318558017c8
Blen <62964622+blenassefa2@users.noreply.github.com>
2022-03-22 12:52:21 +0300

    new

--- week/dq/partitions.py
+++ week/dq/partitions.py
@@ -7,26 +7,24 @@ class Solution:
                 lis[s[i]].append(i)
             else:
                 lis[s[i]][1] = i
-        merged =[]
+        ans =[]
         for i in lis:
-            merged.append(lis[i])
-        for i in range(len(merged)):
-            if len(merged[i]) == 1:
-                merged[i].append(merged[i][0])
-        merged.sort(key = lambda x:(x[0],x[1]))
-
-        ans = [merged[0]]
-        merged= merged[1:]
-        for i in merged:
-           
-            if i[0] > ans[-1][1]:
-                ans.append(i)
-            elif i[0] <= ans[-1][1] and i[1] >= ans[-1][1]:
-                a = [ans[-1][0],i[1]]
-                ans.pop()
-                ans.append(a)
-        final = []
+            if len(lis[i]) == 1:
+                lis[i].append(lis[i][0])
+            ans.append(lis[i])
+        
+        merged = [ans[0]]
+        ans= ans[1:]
         for i in ans:
+           
+            if i[0] > merged[-1][1]:
+                merged.append(i)
+            elif i[0] <= merged[-1][1] and i[1] >= merged[-1][1]:
+                a = [merged[-1][0],i[1]]
+                merged.pop()
+                merged.append(a)
+        final = []
+        for i in merged:
             final.append((i[1] - i[0])+1)
         
         return final
